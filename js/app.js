8e13a4ce056e8b30e8b34ade03555e43281c61a8
tuannv <tuannv@vinasource.com>
2014-11-05 10:06:32 +0700

    - Add video page and fix some feedback

--- js/app.js
+++ js/app.js
@@ -11,15 +11,10 @@
       NProgress.done()
   }, 1500);
 
-  // Toggle Menu
-  $('#settings').on('click', function(e) {
-    e.preventDefault();
-    $('#menu').toggleClass('hidden');
-  });
 
   // Menu Tooltip
   $('.menu-list-link').tooltip();
-  
+
   function centerModal() {
 		$(this).css('display', 'block');
 		var $dialog = $(this).find(".modal-dialog");
@@ -29,4 +24,54 @@
 	}
 
 	$('.modal').on('show.bs.modal', centerModal);
+
+  // Song
+  var $song = $('.song');
+  var $sound = $('.sound');
+  var $jsSoundAction = $('.js-sound-action');
+
+  if ($song.length) {
+    songToggle('.song-like');
+  }
+
+  if ($sound.length) {
+    songToggle('.sound-star');
+
+    $jsSoundAction.on('click', function(e) {
+      e.preventDefault();
+      var $self = $(this);
+
+      if ($self.hasClass('sound-pause')) {
+
+        $self
+          .removeClass('sound-pause')
+          .addClass('sound-play');
+
+        $self
+          .children()
+          .removeClass('fa-pause')
+          .addClass('fa-play');
+      } else {
+        $self
+          .removeClass('sound-play')
+          .addClass('sound-pause');
+
+        $self
+          .children()
+          .removeClass('fa-play')
+          .addClass('fa-pause');
+      }
+    });
+  }
+
+  function songToggle(el) {
+    var $self = $(el);
+
+    $self.on('click', function(e) {
+      e.preventDefault();
+      var $that = $(this);
+
+      $that.toggleClass('active');
+    });
+  }
 }(jQuery));
