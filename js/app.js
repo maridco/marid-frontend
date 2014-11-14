5c51c09e93678a7b73c89944a26aeac054deb3a3
naveen09 <avwna01@AVWNA01-M4800.ca.com>
2014-11-14 10:34:49 +0530

    new changes for settings

--- js/app.js
+++ js/app.js
@@ -1,101 +1,106 @@
 (function($) {
-  'use strict';
+    'use strict';
 
-  // NProgress
-  NProgress.start();
-  NProgress.configure({
-    ease: 'ease',
-    speed: 500
-  });
-  setInterval(function() {
-    NProgress.done()
-  }, 1500);
-
-
-  // Menu Tooltip
-  $('.menu-list-link').tooltip();
-
-  function centerModal() {
-    $(this).css('display', 'block');
-    var $dialog = $(this).find(".modal-dialog");
-    var offset = ($(window).height() - $dialog.height()) / 2;
-		// Center modal vertically in window
-		$dialog.css("margin-top", offset);
-	}
-
-	$('.modal').on('show.bs.modal', centerModal);
-
-  // Following
-  $('.js-follow').on('click', function(event) {
-    event.preventDefault();
-    var $self = $(this);
-
-    $self.find('.unfollow-text').hide();
-    $self.find('.following-text').hide();
-
-    if ($self.hasClass('following')) {
-      $self.removeClass('following');
-    } else {
-      $self.find('.following-text').show();
-      $self.addClass('following');
-    }
-  });
-
-  $(document).on({
-    mouseenter: function () {
-      var $self = $(this);
-      $self.find('.following-text').hide();
-      $self.find('.unfollow-text').show();
-    },
-    mouseleave: function () {
-      var $self = $(this);
-      $self.find('.unfollow-text').hide();
-      $self.find('.following-text').show();
-    }
-  }, '.music-follow.following');
-
-  // Song
-  var $song = $('.song');
-  var $sound = $('.sound');
-  var $jsSoundAction = $('.js-sound-action');
-
-  if ($song.length) {
-    songToggle('.song-like');
-  }
-
-  if ($sound.length) {
-    songToggle('.sound-star');
-
-    $jsSoundAction.on('click', function(e) {
-      e.preventDefault();
-      var $self = $(this);
-
-      if ($self.hasClass('sound-pause')) {
-        $self.removeClass('sound-pause').addClass('sound-play');
-        $self.children().removeClass('fa-pause').addClass('fa-play');
-      } else {
-        $self.removeClass('sound-play').addClass('sound-pause');
-        $self.children().removeClass('fa-play').addClass('fa-pause');
-      }
+    // NProgress
+    NProgress.start();
+    NProgress.configure({
+        ease: 'ease',
+        speed: 500
     });
-  }
+    setInterval(function() {
+        NProgress.done()
+    }, 1500);
 
-  function songToggle(el) {
-    var $self = $(el);
 
-    $self.on('click', function(e) {
-      e.preventDefault();
-      var $that = $(this);
-
-      $that.toggleClass('active');
+    // Menu Tooltip
+    $('.menu-list-link').tooltip();
+    $('.settings li a').on('click', function() {
+        $('.settings li a').each(function(index) {
+            $(this).removeClass("active");
+        });
+        $(this).addClass("active");
     });
-  }
-  $('#disable_notf_btn').on('click', function(e){
-    if ($('#disable_notf_btn').is(":checked"))
-    {
-      $("#notification_form :input").attr("disabled","disabled");
-    }else{
-      $("#notification_form :input").removeAttr("disabled");
+
+    function centerModal() {
+        $(this).css('display', 'block');
+        var $dialog = $(this).find(".modal-dialog");
+        var offset = ($(window).height() - $dialog.height()) / 2;
+        // Center modal vertically in window
+        $dialog.css("margin-top", offset);
     }
-  });
+
+    $('.modal').on('show.bs.modal', centerModal);
+
+    // Following
+    $('.js-follow').on('click', function(event) {
+        event.preventDefault();
+        var $self = $(this);
+
+        $self.find('.unfollow-text').hide();
+        $self.find('.following-text').hide();
+
+        if ($self.hasClass('following')) {
+            $self.removeClass('following');
+        } else {
+            $self.find('.following-text').show();
+            $self.addClass('following');
+        }
+    });
+
+    $(document).on({
+        mouseenter: function() {
+            var $self = $(this);
+            $self.find('.following-text').hide();
+            $self.find('.unfollow-text').show();
+        },
+        mouseleave: function() {
+            var $self = $(this);
+            $self.find('.unfollow-text').hide();
+            $self.find('.following-text').show();
+        }
+    }, '.music-follow.following');
+
+    // Song
+    var $song = $('.song');
+    var $sound = $('.sound');
+    var $jsSoundAction = $('.js-sound-action');
+
+    if ($song.length) {
+        songToggle('.song-like');
+    }
+
+    if ($sound.length) {
+        songToggle('.sound-star');
+
+        $jsSoundAction.on('click', function(e) {
+            e.preventDefault();
+            var $self = $(this);
+
+            if ($self.hasClass('sound-pause')) {
+                $self.removeClass('sound-pause').addClass('sound-play');
+                $self.children().removeClass('fa-pause').addClass('fa-play');
+            } else {
+                $self.removeClass('sound-play').addClass('sound-pause');
+                $self.children().removeClass('fa-play').addClass('fa-pause');
+            }
+        });
+    }
+
+    function songToggle(el) {
+        var $self = $(el);
+
+        $self.on('click', function(e) {
+            e.preventDefault();
+            var $that = $(this);
+
+            $that.toggleClass('active');
+        });
+    }
+    $('#disable_notf_btn').on('click', function(e) {
+        if ($('#disable_notf_btn').is(":checked")) {
+            $("#notification_form :input").attr("disabled", "disabled");
+        } else {
+            $("#notification_form :input").removeAttr("disabled");
+        }
+    });
 }(jQuery));
